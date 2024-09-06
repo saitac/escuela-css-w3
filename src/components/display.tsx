964d1ee18c87b8bb316eabc1eb17c0cf5265ebb9
Salvador Aita Campodonico <salvadoraita@gmail.com>
2024-09-06 19:01:25 -0400

    20240906 19:00

--- src/components/display.tsx
+++ src/components/display.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react"
 import "../css/display.css"
 
 const Display = () => {
+
+    const [muestra, setMuestra] = useState(false);
+
     return(
         <section>
-            <div className="principal muestra">
-                <div className="titulo" onClick={()=>console.log("OK!")}>
+            <div className="principal">
+                <div className="titulo" onClick={()=>setMuestra(!muestra)}>
                     <p>Click para <span>mostrar el panel</span></p>
                 </div>
-                <div className="contenido">
+                <div className={"contenido" + (muestra ? " muestra" : " no_muestra")}>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum inventore, maiores doloribus tempore cumque dolore excepturi quo vero iste ipsa hic quos? Recusandae temporibus quod assumenda repellat culpa nesciunt necessitatibus?</p>
                 </div>
             </div>
